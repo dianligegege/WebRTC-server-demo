18b02f1ae4f4832883bfbc4cd264abefcded6c65
dianligegege <1220671124@qq.com>
2024-01-17 16:55:18 +0800

    fix: 修改跨域

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { createServer } = require('https');
 const { Server } = require('socket.io');
-const path = require('path');
+// const path = require('path');
 // const fs = require('fs');
 // const { fileURLToPath } = require('url');
-const cors = require('cors');
+// const cors = require('cors');
 
 const MAX_USER_COUNT = 2;
 
@@ -18,13 +18,19 @@ const options = {
 };
 
 const app = express();
-app.use(express.static(path.join(__dirname, "./")));
-app.use(cors({
-  origin: "*",
-  methods: ['GET', 'POST'],
-  allowedHeaders: '*',
-  credentials: false,
-}));
+// app.use(express.static(path.join(__dirname, "./")));
+//设置跨域访问
+app.all('*', (req, res, next) => {
+  //设置允许跨域的域名，*代表允许任意域名跨域
+  res.header('Access-Control-Allow-Origin', '*')
+  //允许的header类型
+  res.header('Access-Control-Allow-Headers', 'content-type')
+  //跨域允许的请求方式
+  res.header('Access-Control-Allow-Methods', 'DELETE,PUT,POST,GET,OPTIONS')
+  //让options尝试请求快速结束
+  if (req.method.toLowerCase() == 'options') res.send(200)
+  else next()
+})
 
 const https = createServer(options, app);
 
